f6721b0fea9b165cb65eb05444fbbd167a3b2200
lubegasimon <lubegasimon73@gmail.com>
2024-10-11 00:05:39 +0300

    basic server logging on incoming requests

--- server/server.ml
+++ server/server.ml
@@ -7,7 +7,10 @@ let server =
   listen server_sock 10;
   let rec accept_loop () =
     accept server_sock >>= fun (client_sock, _client_addr) ->
+    let in_chan = Lwt_io.of_fd ~mode:Lwt_io.Input client_sock in
     let out_chan = Lwt_io.of_fd ~mode:Lwt_io.Output client_sock in
+    let%lwt incoming_req = Lwt_io.read ~count:1024 in_chan in
+    Lwt_io.printl incoming_req >>= fun () ->
     let response_str =
       "HTTP/1.1 200 OK\r\n\
        Content-Length: 11\r\n\
