809fdbea0265eeba87f3be28ab0ca3bf78bcf86c
lubegasimon <lubegasimon73@gmail.com>
2024-10-10 18:35:52 +0300

    clean up Response.of_string function

--- lib/http_.ml
+++ lib/http_.ml
@@ -66,6 +66,12 @@ module Status = struct
     | `OK -> "OK"
     | `Continue -> "Continue"
     | `Switching_protocols -> "Switching_protocols"
+
+  let msg_of_code = function
+    | `Code 200 -> `OK
+    | `Code 100 -> `Continue
+    | `Code 101 -> `Switching_protocols
+    | _ -> failwith "Unimplemented status code"
 end
 
 module Response = struct
@@ -77,43 +83,39 @@ module Response = struct
     body : string option;
   }
 
+  let parse_headers rest =
+    let rec aux acc = function
+      | "\r" :: xs -> (List.rev acc, xs (* "\r" denotes end of header section *))
+      (* Header section *)
+      | x :: xs -> aux ((String.trim x |> Header.of_string) :: acc) xs
+      | [] -> ([], [])
+    in
+    aux [] rest
+
+  let get_status_line_parts status_line =
+    let line_parts = String.(split_on_char ' ' (trim status_line)) in
+    match line_parts with
+    (* we ignore msg because we can compute it from the code *)
+    | version :: code :: _msg ->
+        let version = Version.of_string version in
+        let code = `Code (int_of_string code) in
+        let msg = Status.msg_of_code code in
+        (version, code, msg)
+    | _ -> failwith "Invalid status line"
+
   let of_string res =
     let lines = String.split_on_char '\n' res in
     match lines with
     | [] -> failwith "Empty response"
     | status_line :: rest ->
-        let line_components =
-          String.trim status_line |> String.split_on_char ' '
+        let version, status_code, status_msg =
+          get_status_line_parts status_line
         in
-        let version, code =
-          match line_components with
-          (* we ignore msg because we can compute it from the code *)
-          | version :: code :: _msg -> (version, code)
-          | _ -> failwith "Invalid status line"
+        let headers, body =
+          let headers, body = parse_headers rest in
+          let headers = match headers with [] -> None | xs -> Some xs in
+          let body = Some (String.concat "" body) in
+          (headers, body)
         in
-        let parse_headers rest =
-          let rec aux acc = function
-            | "\r" :: xs ->
-                (List.rev acc, xs (* "\r" denotes end of header section *))
-            (* Header section *)
-            | x :: xs -> aux ((String.trim x |> Header.of_string) :: acc) xs
-            | [] -> ([], [])
-          in
-          aux [] rest
-        in
-        let headers, body = parse_headers rest in
-        let headers =
-          match headers with [] -> None | headers -> Some headers
-        in
-        let version = Version.of_string version in
-        let status_code = `Code (int_of_string code) in
-        let status_msg =
-          match status_code with
-          | `Code 200 -> `OK
-          | `Code 100 -> `Continue
-          | `Code 101 -> `Switching_protocols
-          | _ -> failwith "Unimplemented status code"
-        in
-        let body = Some (String.concat "" body) in
         { version; status_code; status_msg; headers; body }
 end
