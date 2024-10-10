d1e2b80841dea8d2d9ae36cd0a75d9e4626f3223
lubegasimon <lubegasimon73@gmail.com>
2024-10-10 16:25:28 +0300

    nitpick: rm more redundant code

--- client/client.ml
+++ client/client.ml
@@ -1,8 +1,8 @@
 open Http_
-open Lwt
+open Lwt.Infix
+open Lwt_unix
 
 let connect_to_server =
-  let open Lwt_unix in
   let sock = socket PF_INET SOCK_STREAM 0 in
   let addr = ADDR_INET (Unix.inet_addr_loopback, 5000) in
   connect sock addr >>= fun _ -> Lwt.return sock
@@ -10,12 +10,12 @@ let connect_to_server =
 let send_request sock req =
   let req_str = Request.to_string req in
   let req_bytes = Bytes.of_string req_str in
-  Lwt_unix.write sock req_bytes 0 (Bytes.length req_bytes)
+  write sock req_bytes 0 (Bytes.length req_bytes)
 
 let receive_response sock =
   let buf_size = 4096 in
   let buf = Bytes.create buf_size in
-  let%lwt bytes_read = Lwt_unix.read sock buf 0 buf_size in
+  let%lwt bytes_read = read sock buf 0 buf_size in
   if bytes_read == 0 then Lwt.return (Bytes.to_string Bytes.empty)
   else
     let contents = Bytes.sub_string buf 0 bytes_read in
@@ -25,7 +25,7 @@ let send_http_request request =
   connect_to_server >>= fun sock ->
   send_request sock request >>= fun _ ->
   receive_response sock >>= fun res_str ->
-  Lwt_unix.close sock >>= fun _ -> Lwt.return (Response.of_string res_str)
+  close sock >>= fun _ -> Lwt.return (Response.of_string res_str)
 
 let () =
   let request =
